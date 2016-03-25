2d2d360aafe790623dba3f36bfab48ae2aad0ee1
alhymov <alhymov@mail.ru>
2016-03-25 04:51:28 +0300

    Create

---
+++ uMain.pas
@@ -0,0 +1,56 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Objects, FMX.Ani;
+
+type
+  TForm10 = class(TForm)
+    Rectangle1: TRectangle;
+    Layout1: TLayout;
+    Rectangle2: TRectangle;
+    Label1: TLabel;
+    Rectangle3: TRectangle;
+    Label2: TLabel;
+    Rectangle4: TRectangle;
+    Label3: TLabel;
+    Rectangle5: TRectangle;
+    Label4: TLabel;
+    Rectangle6: TRectangle;
+    Label5: TLabel;
+    Rectangle7: TRectangle;
+    Label6: TLabel;
+    Rectangle8: TRectangle;
+    Label7: TLabel;
+    Rectangle9: TRectangle;
+    Label8: TLabel;
+    Rectangle10: TRectangle;
+    Label9: TLabel;
+    Rectangle11: TRectangle;
+    Label10: TLabel;
+    Rectangle12: TRectangle;
+    Label11: TLabel;
+    Rectangle13: TRectangle;
+    Label12: TLabel;
+    Label13: TLabel;
+    Rectangle14: TRectangle;
+    Rectangle15: TRectangle;
+    FloatAnimation1: TFloatAnimation;
+    FloatAnimation2: TFloatAnimation;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form10: TForm10;
+
+implementation
+
+{$R *.fmx}
+
+end.
